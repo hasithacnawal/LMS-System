35bb954551d3d5ce908e33d8340848f1c3c387be
Chana96 <ahasithachandana@gmail.com>
2021-05-15 16:41:26 +0530

    register error resolved

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,53 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { error } from 'console';
 import { AuthService } from 'src/app/auth.service';
 import { RegisterPayload } from '../register-payload';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  
-  registerForm: FormGroup ;
+  registerForm: FormGroup;
   registerPayload: RegisterPayload;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
-    this.registerForm= this.formBuilder.group({
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.registerForm = this.formBuilder.group({
       userName: '',
       email: '',
       password: '',
-      confirmPassword: ''
+      confirmPassword: '',
     });
-    this.registerPayload ={
+    this.registerPayload = {
       userName: '',
       email: '',
       password: '',
-      confirmPassword: ''
+      confirmPassword: '',
     };
-
-    }
-   }
-
-  ngOnInit():void {
-    
-   } 
-
-  onSubmit(){
-    this.registerPayload.userName= this.registerForm.get('userName').value;
-    this.registerPayload.email= this.registerForm.get('email').value;
-    this.registerPayload.password = this.registerForm.get('password').value;
-    this.registerPayload.confirmPassword = this.registerForm.get('confirmPassword').value;
-
-   this.authService.register(this.registerPayload).subscribe(data =>{
-     console.log('register succes');
-   }, error=>{
-     console.log('register failed')
-   })
   }
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.registerPayload.userName = this.registerForm.get('userName').value;
+    this.registerPayload.email = this.registerForm.get('email').value;
+    this.registerPayload.password = this.registerForm.get('password').value;
+    this.registerPayload.confirmPassword =
+      this.registerForm.get('confirmPassword').value;
+
+    this.authService.register(this.registerPayload).subscribe(
+      (data) => {
+        console.log('register succes');
+      },
+      (error) => {
+        console.log('register failed');
+      }
+    );
+  }
 }
- 
