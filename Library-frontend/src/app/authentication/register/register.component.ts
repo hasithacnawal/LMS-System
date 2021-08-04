4932b31921ae5f78ed169c73e0ba50f5ab1a5458
chana96 <ahasithachandana@gmail.com>
2021-08-04 18:58:26 +0530

    confirm password conflic resolved

--- Library-frontend/src/app/authentication/register/register.component.ts
+++ Library-frontend/src/app/authentication/register/register.component.ts
@@ -1,8 +1,10 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/core/service/auth.service';
 import { RegisterPayload } from '../register-payload';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
@@ -16,18 +18,21 @@ export class RegisterComponent implements OnInit {
   returnUrl: string;
   hide = true;
   chide = true;
+  password: string;
+  confirmPassword: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router,
+    private snackBar: MatSnackBar,
     private route: ActivatedRoute
   ) {
     this.registerForm = this.formBuilder.group({
       firstName: [''],
-      lastName: '',
-      userName: ['', [Validators.required]],
-      email: ['', [Validators.required]],
+      lastName: [''],
+      phone: [''],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
       confirmPassword: ['', [Validators.required]],
       roleId: [2],
@@ -35,16 +40,34 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit(): void {}
-
+  showNotification(colorName, text, placementFrom, placementAlign) {
+    this.snackBar.open(text, '', {
+      duration: 2000,
+      verticalPosition: placementFrom,
+      horizontalPosition: placementAlign,
+      panelClass: colorName,
+    });
+  }
   onSubmit() {
     this.submitted = true;
-    this.authService.register(this.registerForm.value).subscribe(
-      (data) => {
-        this.router.navigateByUrl('/authentication/signin');
-      },
-      (error) => {
-        console.log('register failed');
-      }
-    );
+    this.password = this.registerForm.value.password;
+    this.confirmPassword = this.registerForm.value.confirmPassword;
+    if (this.password == this.confirmPassword) {
+      this.authService.register(this.registerForm.value).subscribe(
+        (data) => {
+          this.router.navigateByUrl('/authentication/signin');
+        },
+        (error) => {
+          console.log('register failed');
+        }
+      );
+    } else {
+      this.showNotification(
+        'snackbar-danger',
+        'Retype the confirm password',
+        'bottom',
+        'center'
+      );
+    }
   }
 }
